6fc70a8ec028dd6f1ddd624476e3296e32b5f2d3
juan david marin <juaanmarin2007@gmail.com>
2022-05-13 13:07:22 -0500

    agrego el proyecto base de la pagina home page on react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import {ButtonUI} from "./components/UI/buttonUI/ButtonUI"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">welcome to home page on react</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+      <section className="App-section">
+        <h2 className="secondTitle">hellow</h2>
+        <ButtonUI></ButtonUI>
+        <p className="text">Liber nonumy ipsum. Dolores sit sed suscipit magna elitr sit cum et takimata. Est qui magna diam invidunt kasd dolor in sed gubergren tempor sit lorem diam accusam sit gubergren elitr ea. Elitr et dolores magna et sea lorem. Consetetur nulla vero kasd et kasd vero laoreet labore diam feugait diam tempor magna. Odio rebum lorem tincidunt dolore takimata feugait sadipscing clita dolor lorem sit. Dolor lorem lorem sed eum nulla adipiscing. Molestie elitr vel kasd praesent duo duo et consequat elitr dolor gubergren tempor sit amet labore ut zzril. Ea ipsum amet stet nulla te eum accusam labore eos vel dolore kasd amet invidunt amet iusto sit rebum. Sed placerat kasd duis velit ut diam voluptua accusam enim justo amet ea dolore sed eirmod sed erat dolore. Elitr dolor duo te justo. Et kasd justo amet at et eirmod option diam kasd hendrerit labore at rebum ut. Dolore eleifend lorem et et et accusam ipsum no.</p>
+      </section>
+
+      <footer>
+        <div className="contact">
+          <h2>@juaanmarin2007@gmail.com</h2>
+        </div>
+        <div className="icons">
+            
+        </div>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/buttonUI/ButtonUI.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ButtonUI = () => {
+  return (
+    <button>buttonUi</button>
+  )
+}
