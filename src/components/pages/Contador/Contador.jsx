464179084401e935a3787895363d5b0d37978267
juan david marin <juaanmarin2007@gmail.com>
2022-05-23 15:34:30 -0500

    se agrego el sol a la pagina contador cuando el contador llega a 100

--- src/components/pages/Contador/Contador.jsx
+++ src/components/pages/Contador/Contador.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Footer } from '../../layouts/Footer/Footer'
 import { HeaderHome } from '../../layouts/HeaderHome/HeaderHome'
 import { NavHome } from '../../layouts/NavHome/NavHome'
 import { ButtonUI } from '../../UI/buttonUI/ButtonUI'
 import { Title } from '../../UI/TitleP/Title'
+import sol from "../../../images/dom.png"
+import solD from "../../../images/dom1.png"
 
 export const Contador = () => {
 
   const [num,setNum]=useState(0);
 
+  const [cont, setCont] = useState(1);
+
+  useEffect(() => {
+
+    const image=document.getElementById("image");
+
+    if(num>99){
+      image.setAttribute("src",solD );
+    }
+    else{
+      image.removeAttribute("src");
+      image.setAttribute("src",sol );
+    }
+  }, [num])
+    
+
+  const cambio=( {target} )=>{
+    setCont(parseInt(target.value))
+  }
+  
   return (
     <>
     <div className="App-header">
@@ -19,12 +41,23 @@ export const Contador = () => {
         <NavHome></NavHome>
     </div>
     <div className='App-section'>
-        <h2 className='resultado'>{num}</h2>
-        <div className='botones'>
-          <ButtonUI classBtn="mas" event={()=>setNum(num+1)} textName="mas +"></ButtonUI>
-          <ButtonUI classBtn="menos" event={()=>setNum(num-1)} textName="menos -"></ButtonUI>
-          <ButtonUI classBtn="restaurar" event={()=>setNum(0)} textName="restaurar 0"></ButtonUI>
-        </div>     
+        <select onChange={cambio} name="" id="select">
+          <option id='options' value="1">contador 1</option>
+          <option id='options' value="10">contador 10</option>
+        </select>
+        <div className="contador">
+          <h2 className='resultado'>{num}</h2>
+          <div className='botones'> 
+            <ButtonUI classBtn="mas" event={()=>setNum(num+cont)} textName="mas +"></ButtonUI>
+            <ButtonUI classBtn="menos" event={()=>setNum(num-cont)} textName="menos -"></ButtonUI>
+            <ButtonUI classBtn="restaurar" event={()=>setNum(0)} textName="restaurar 0"></ButtonUI>
+          </div>  
+        </div>
+        
+        <div className='card'>
+          <img id='image' src="" alt="sol" />
+        </div>
+
     </div>
     
     <Footer></Footer>
